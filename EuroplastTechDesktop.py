f335fbc99be727d8e8acfee60d948c64e87dc596
yudin <brawlakkum@gmail.com>
2024-10-06 12:57:48 +0300

    update main file

--- EuroplastTechDesktop.py
+++ EuroplastTechDesktop.py
@@ -1,54 +1,73 @@
 import sys
 from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QHBoxLayout, 
-                             QComboBox, QLabel, QPushButton, QFileDialog, QGridLayout,
-                             QDialog, QDialogButtonBox)
-from parseExcel import plot_histogram_to_excel
+                             QLabel, QPushButton, QDialog, QDialogButtonBox, 
+                             QMainWindow, QListView, QPushButton, QSizePolicy)
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+from PyQt5.QtCore import QModelIndex
+from DB import get_current_tasks
+from dataModels.tasksDataModel import Tasks, Task
 
-class EuroplastTechDesktop(QWidget):
+
+class EuroplastTechDesktop(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.selected_file = None
-
         self.setWindowTitle("EuroplastTechDesktop")
+        self.setGeometry(100, 100, 800, 600)
 
-        self.podrazdelenie = QComboBox()
-        self.podrazdelenie.setCurrentText("")
-        self.podrazdelenie.addItems(["Цех 5", "ОГЭ"])
+        centralWidget = QWidget()
+        self.setCentralWidget(centralWidget)
 
-        self.label = QLabel("Podrazdelenie")
-        self.label.setMaximumWidth(200)
+        self.mainLayout = QHBoxLayout(centralWidget)
 
-        self.file_button = QPushButton("Выбрать файл")
-        self.file_button.clicked.connect(self.open_file_dialog)
+        self.burgerButton = CustomBtn("☰")
+        self.burgerButton.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.burgerButton.clicked.connect(self.toggle_menu)
 
-        self.file_label = QLabel("Выберите файл")
-        self.file_label.setMaximumWidth(200)
+        self.mainLayout.addWidget(self.burgerButton)
 
-        layout = QGridLayout()
-        layout.addWidget(self.label, 0, 0)
-        layout.addWidget(self.podrazdelenie, 0, 1)
-        layout.addWidget(self.file_label, 1, 0)
-        layout.addWidget(self.file_button, 1, 1)
+        self.menuContainer = QWidget()
+        self.menuLayout = QVBoxLayout(self.menuContainer)
 
-        self.setLayout(layout)
-        self.resize(900, 600)
-        self.move(500, 300)
+        self.menuLayout.setSpacing(20)
+        self.menuButton1 = QPushButton("Меню 1")
+        self.menuButton2 = QPushButton("Меню 2")
+        self.menuButton3 = QPushButton("Меню 3")
 
-    def open_file_dialog(self):
-        options = QFileDialog.Options()
-        file_name, _ = QFileDialog.getOpenFileName(self, "Выберите файл", "", "*.xlsx", options=options)
-        if file_name:
-            self.selected_file = file_name
-            try:
-                plot_histogram_to_excel(self.selected_file)
-                dialog = AlertDialog("""
-Файл успешно изменён!
-Графики добавлены на новый лист в Вашем файле!""")
-                dialog.exec()
-            except Exception as e:
-                dialog = AlertDialog(f"Ошибка! {e}")
-                dialog.exec()
+        self.menuLayout.addWidget(self.menuButton1)
+        self.menuLayout.addWidget(self.menuButton2)
+        self.menuLayout.addWidget(self.menuButton3)
+        self.menuLayout.addStretch(100)
+        self.menuContainer.setFixedWidth(150)
+        self.menuContainer.hide()
+
+        self.mainLayout.addWidget(self.menuContainer)
+
+        self.tasksListView = QListView()
+        self.tasksModel = QStandardItemModel()
+        self.tasksListView.setModel(self.tasksModel)
+
+        self._data = get_current_tasks()
+        self.tasks = [Task(**task_data) for task_data in self._data]
+
+        for task in self.tasks:
+            item = QStandardItem(task.task)
+            self.tasksModel.appendRow(item)
+
+        self.tasksListView.clicked.connect(self.on_task_selected)
+
+        self.mainLayout.addWidget(self.tasksListView)
+
+    def toggle_menu(self):
+        if self.menuContainer.isVisible():
+            self.menuContainer.hide()
+        else:
+            self.menuContainer.show()
+
+    def on_task_selected(self, index: QModelIndex):
+        if index.isValid():
+            selected_task = self.tasks[index.row()]
+            print(f"Выбрана задача: {selected_task.task}, статус: {selected_task.status}")
 
 
 class AlertDialog(QDialog):
@@ -68,7 +87,20 @@ class AlertDialog(QDialog):
         layout.addWidget(self.buttonBox)
         self.setLayout(layout)
 
-app = QApplication(sys.argv)
-window = EuroplastTechDesktop()
-window.show()
-sys.exit(app.exec())
+class CustomBtn(QPushButton): 
+    def __init__(self, text: str = 'btn', width: int = 100, height: int = 50):
+        super().__init__()
+
+        self.setStyleSheet(
+            "QPushButton {background-color: rgb(255, 0, 255); border-radius: 20px;}"
+        )
+        self.setText(text)
+        self.setFixedSize(width, height)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = EuroplastTechDesktop()
+    window.show()
+    sys.exit(app.exec())
+    
--- parseExcel.py
+++ parseExcel.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import pandas as pd # type: ignore
 from datetime import datetime
 import os
 import matplotlib.pyplot as plt
